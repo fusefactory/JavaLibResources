9b8caccf871309b5cba02d2cff25dd5e0793f070
mark <mark.korput@gmail.com>
2019-02-27 23:33:27 +0100

    example app tests consecutive audio loads

--- example/src/main/java/com/fuse/ui/example/App.java
+++ example/src/main/java/com/fuse/ui/example/App.java
@@ -4,6 +4,8 @@ import java.util.logging.*;
 import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.core.PVector;
+import com.fuse.resources.AudioSource;
+import ddf.minim.AudioPlayer;
 
 public class App extends PApplet {
   Logger logger;
@@ -11,6 +13,8 @@ public class App extends PApplet {
   private PGraphics pg;
   private float timeBetweenFrames;
   private boolean bDrawDebug;
+
+  private AudioSource audioSource;
 
   public static void main( String[] args )
   {
@@ -32,6 +36,8 @@ public class App extends PApplet {
     papplet.frameRate(30.0f);
     timeBetweenFrames = 1.0f / papplet.frameRate;
     bDrawDebug = false;
+
+    this.audioSource = new AudioSource(this.papplet);
   }
 
   private void update(float dt){
@@ -57,10 +63,28 @@ public class App extends PApplet {
     switch(key){
       case '1': {
         // load
+        this.logger.info("load1");
+        this.audioSource.getAsync(dataPath("test.mp3")).withSingleResult((AudioPlayer player) -> {
+          this.logger.info("load1-done");
+          player.play();
+          this.logger.info("load1 started");
+          player.close();
+          this.logger.info("load1 closed");
+        });
+        return;
       }
 
       case '2': {
         // load
+        this.logger.info("load2");
+        this.audioSource.getAsync(dataPath("test.mp3")).withSingleResult((AudioPlayer player) -> {
+          this.logger.info("load2-done");
+          player.play();
+          this.logger.info("load2 started");
+          player.close();
+          this.logger.info("load2 closed");
+        });
+        return;
       }
 
       case 'd': {
